7ec11c50545dedbaa2486980d777bdbcb9cecc8a
Robert812218 <robert812@shaw.ca>
2023-04-03 12:11:46 -0600

    add NetCat and send methods

--- netcat.py
+++ netcat.py
@@ -6,6 +6,44 @@ import sys
 import textwrap
 import threading
 
+def send(self):
+    self.socket.connect((self.args.target, self.args.port))
+    if self.buffer:
+        self.socket.send(self.buffer)
+
+    try:
+        while True:
+            recv_len = 1
+            response = ''
+            while recv_len:
+                data = self.socket.recv(4096)
+                recv_len = len(data)
+                response += data.decode()
+                if recv_len < 4096:
+                    break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
+class NetCat:
+    def __init__(self, args, buffer=None)
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REFUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
@@ -38,3 +76,4 @@ def execute(cmd):
                     nc = NetCat(args, buffer.encode())
                     nc.run()
                 )
+
