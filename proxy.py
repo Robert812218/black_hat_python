96ef9e1a473bcc86ebd26f90a47b850d55a33712
GOURLAMI <robert812@shaw.ca>
2023-04-05 10:22:54 -0600

    add proxy handler

--- proxy.py
+++ proxy.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import threading
 
+
 HEX_FILTER = ''.join(
         [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
@@ -22,3 +23,46 @@ def hexdump(src, length=16, show=True):
             print(line)
         else:
             return results
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+        except Exception as e:
+            pass
+        return buffer
+
+def request_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+    remote_buffer = response_handler(remote_buffer)
+    
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+        remote_buffer = response_handler(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data. Closing connections.")
+            break
+
+
